0c5c8396e57e335278af2b1dc8d8fa9de63c8170
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-24 09:54:22 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,43 @@
 class DogsController < ApplicationController
+
+    def index 
+       @dogs = Dog.all 
+    end
+
+    def new 
+        @dog = Dog.new
+    end
+
+    def create 
+        @dog = Dog.create(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all.select{ |employee| employee.dog_id == @dog.id}
+    end
+
+    def edit 
+        @dog = Dog.find(params[:id])
+    end
+
+    def update 
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    def delete 
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_path
+    end
+
+    private
+
+    def dog_params
+        params.require(:dogs).permit(:name, :breed, :age)
+    end
+
 end
