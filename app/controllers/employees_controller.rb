d8c7ada42aab201364a153ef42d935fee7c9ebb3
steve-alex <steve.alex@hotmail.co.uk>
2019-09-25 09:41:38 +0100

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,46 @@
 class EmployeesController < ApplicationController
+
+  def new
+    @employee = Employee.new
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def index
+    @employees = Employee.all
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit!
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def reorder
+      @dogs = Dog.all.sort_by{ |dog| dog.employees.length }
+    end
+
 end
