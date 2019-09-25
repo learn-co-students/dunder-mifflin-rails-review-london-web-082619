ab49ff72b0a895e0bd22e0615ba2795bc2a03d36
github username <github email address>
2019-09-25 09:48:12 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :dogs
+  resources :dogs, only: [:index, :show]
   resources :employees
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "dogs", to: "dogs#reorder", as: "reorder_dogs"
 end
